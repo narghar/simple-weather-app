43fef8c35495733857df9fbfb487f610a89af7ba
Igor <narghar@gmail.com>
2019-11-26 18:19:07 +0100

    Clear

--- app.js
+++ app.js
@@ -22,12 +22,4 @@ app.post('/api/weather/', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}!`)
-})
-
-
-
-// request('http://www.google.com', function (error, response, body) {
-//   console.error('error:', error); // Print the error if one occurred
-//   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-//   console.log('body:', body); // Print the HTML for the Google homepage.
-// });
+});
