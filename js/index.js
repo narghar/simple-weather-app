5caf280ff9c79dba25916e740683e7b257c52f36
Igor <narghar@gmail.com>
2019-12-02 09:48:25 +0100

    Clear input on focus

--- js/index.js
+++ js/index.js
@@ -1,8 +1,19 @@
-
 const geolocation = require("./modules/geolocation");
 const Prompt = require("./modules/prompt");
+const input = document.getElementById('search-city');
+
+
 
 geolocation();
+input.addEventListener('focus', (event) => {
+   event.target.dataset.value = event.target.value;
+   event.target.value = '';
+
+})
+input.addEventListener('blur', (event) => {
+  event.target.value = event.target.dataset.value;
+  event.target.dataset.value = '';
+})
 
 
 Prompt.loadGoogleMapsApi().then(function(googleMaps) {
